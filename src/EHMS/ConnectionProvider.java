5b310a2075aa4f5aa8f31dffd7b900e4b41346c1
yyash01 <yjain3.436@gmail.com>
2020-11-07 23:38:47 +0530

    Changed to Useful situation

--- src/EHMS/ConnectionProvider.java
+++ src/EHMS/ConnectionProvider.java
@@ -4,22 +4,26 @@ public class ConnectionProvider {
 	public static Connection getCon()
 	{
 		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			
 			String url="jdbc:mysql://localhost:3306/HealthcareMangaementSystem";
 			String uname="root";
-			String pass="jain@123";
-			String query = "select ContactNumber from Patients where PatientID = 1";
-			Class.forName("com.mysql.jdbc.Driver");
+			String pass="root123@";
+//			String query = "select ContactNumber from Patients where PatientID = 1";
 			Connection con=DriverManager.getConnection(url,uname,pass);
-			Statement st = con.createStatement();
-			ResultSet rs =  st.executeQuery(query);
-			rs.next();
+//			System.out.println("CONNECTION");
+//			Statement st = con.createStatement();
+//			ResultSet rs =  st.executeQuery(query);
+//			rs.next();
 			
-			String name = rs.getString("ContactNumber");
-			System.out.print(name);
+//			String name = rs.getString("ContactNumber");
+//			System.out.println(name);
+			
+//			System.out.println("Successful");
 			return con;
-		}catch(Exception e) {
+		}
+		catch(Exception e) {
 			return null;
 		}
 	}
-
 }
